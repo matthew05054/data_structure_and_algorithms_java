724db5a49e075ce9f99c53a194d3e5fc4c27839c
MATTHEW <matthewmansah@gmail.com>
2026-06-04 22:49:07 +0530

    Final changes in thread first program

--- Threads/CodeEditor.java
+++ Threads/CodeEditor.java
@@ -1,16 +1,30 @@
 package threadsDemo;
 
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+// DateTimeFormatter formatter = DateTimeFormatter.ofPatter("HH:mm:sss");
+
+
 class AutoSave extends Thread{
+
+	private static final DateTimeFormatter formatter =
+	DateTimeFormatter.ofPattern("HH:mm:ss");
 	public void run() {
 		System.out.println(Thread.currentThread().getName()
 				+": " + Thread.currentThread().getPriority());
+
+		// Date date = new Date();
+		// time_ = date.now();
 		
-		Thread.currentThread().setPriority(MAX_PRIORITY);
+		Thread.currentThread().setPriority(MIN_PRIORITY);
+		System.out.println(Thread.currentThread().getPriority());
 		
 		for(int i=0;i<5;i++) {
 			try {
 				Thread.sleep(3000);
-				System.out.println("Saving code!");
+				System.out.println(LocalTime.now().format(formatter)  + " Saving code!");
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -21,13 +35,16 @@ class AutoSave extends Thread{
 
 public class CodeEditor {
 
+
+	private static final DateTimeFormatter formatter =
+	DateTimeFormatter.ofPattern("HH:mm:ss");
 	public static void main(String[] args) throws InterruptedException {
 		AutoSave autoSave = new AutoSave();
 		autoSave.start();
 		
 		for(int i=0;i<10;i++) {
 			Thread.sleep(2000);
-			System.out.println("Typing code...");
+			System.out.println(LocalTime.now().format(formatter) + " Typing code...");
 		}
 		
 		
